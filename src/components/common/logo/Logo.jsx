e35fa0710fb82cfc6b12134b5ce6b98d7f07764a
donginLee <donginyee@gmail.com>
2024-06-12 11:09:35 +0900

    ✨feat: files - logo 이미지 클릭하면 링크로 이동할 수 있는 컴포넌트 구현

---
+++ src/components/common/logo/Logo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyledLogoImg } from "./Logo.style";
+
+export default function Logo({ size }) {
+  return (
+    <a href="#">
+      <StyledLogoImg size={size} src="public/assets/images/logo.png" />
+    </a>
+  );
+}
