c8c6e0de2f8addb281f5b890ae3af5e1592e822a
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-06-12 14:56:23 +0900

    ✨ feat: 사이드바 컴포넌트 구현 완료

--- src/components/common/sidebar/Sidebar.jsx
+++ src/components/common/sidebar/Sidebar.jsx
@@ -1,3 +1,31 @@
+import {
+  StyledSidebarDiv,
+  StyledSidebarItemDiv,
+  StyledHomeIcon,
+  StyledAnalyzeIcon,
+  StyledRelateIcon,
+  StyledKeywordIcon,
+} from "./Sidebar.style";
+
 export default function Sidebar() {
-  return <div></div>;
+  return (
+    <StyledSidebarDiv>
+      <StyledSidebarItemDiv>
+        <StyledHomeIcon />
+        <span>한눈에 보기</span>
+      </StyledSidebarItemDiv>
+      <StyledSidebarItemDiv>
+        <StyledAnalyzeIcon />
+        <span>소셜 분석</span>
+      </StyledSidebarItemDiv>
+      <StyledSidebarItemDiv>
+        <StyledRelateIcon />
+        <span>연관 주식</span>
+      </StyledSidebarItemDiv>
+      <StyledSidebarItemDiv>
+        <StyledKeywordIcon />
+        <span>연관 키워드</span>
+      </StyledSidebarItemDiv>
+    </StyledSidebarDiv>
+  );
 }
