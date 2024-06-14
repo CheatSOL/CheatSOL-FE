e17ad00124a70656407ddb4f616da759089bdd52
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-06-14 15:31:09 +0900

    🐛 fix: Logo 컴포넌트 경로 수정

--- src/components/common/logo/Logo.jsx
+++ src/components/common/logo/Logo.jsx
@@ -4,7 +4,7 @@ import { StyledLogoImg } from "./Logo.style";
 export default function Logo({ size }) {
   return (
     <a href="/">
-      <StyledLogoImg size={size} src="public/assets/images/logo.png" />
+      <StyledLogoImg size={size} src="/assets/images/logo.png" />
     </a>
   );
 }
