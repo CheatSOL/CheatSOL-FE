389fdc1d49572e09edced7a60fee44d83f10a558
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-06-14 15:30:34 +0900

    ✨ feat: 소셜 페이지 컴포넌트 위치 구현 완료

--- src/pages/social/Social.jsx
+++ src/pages/social/Social.jsx
@@ -1,8 +1,19 @@
-import YoutubeData from "~/components/common/news/youtube-data/Youtube.data";
+import Header from "../../components/common/header/Header";
+import GoogleNews from "../../components/common/news/google-news/Google.news";
+import Sidebar from "../../components/common/sidebar/Sidebar";
+import GoogleItem from "./Google.item";
+import { StyledSocialDiv, StyledSocialInfoDiv } from "./Social.style";
+import YoutubeItem from "./Youtube.item";
 
 export default function SocialPage() {
-  return <div>
-    socialPage
-    <YoutubeData></YoutubeData>
-    </div>;
+  return (
+    <StyledSocialDiv>
+      <Sidebar />
+      <StyledSocialInfoDiv>
+        <Header />
+        <GoogleItem />
+        <YoutubeItem />
+      </StyledSocialInfoDiv>
+    </StyledSocialDiv>
+  );
 }
