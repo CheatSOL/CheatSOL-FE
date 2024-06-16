0f3491eb09d62b99f111f156f623b0e734121d3e
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-06-16 11:35:34 +0900

    ✨feat: scroll top button 생성 완료

--- src/pages/social/Social.jsx
+++ src/pages/social/Social.jsx
@@ -1,3 +1,5 @@
+// src/pages/SocialPage.js
+import React, { useRef } from "react";
 import Header from "../../components/common/header/Header";
 import Sidebar from "../../components/common/sidebar/Sidebar";
 import GoogleItem from "./Google.item";
@@ -5,18 +7,22 @@ import InstagramItem from "./Instagram.item";
 import NaverItem from "./NaverItem";
 import { StyledSocialDiv, StyledSocialInfoDiv } from "./Social.style";
 import YoutubeItem from "./Youtube.item";
+import ScrollToTopButton from "../../components/common/scroll-top-button/Scroll.top.button";
 
 export default function SocialPage() {
+  const scrollRef = useRef(null);
+
   return (
     <StyledSocialDiv>
       <Sidebar />
-      <StyledSocialInfoDiv>
+      <StyledSocialInfoDiv ref={scrollRef}>
         <Header />
         <GoogleItem />
         <YoutubeItem />
         <InstagramItem />
         <NaverItem />
       </StyledSocialInfoDiv>
+      <ScrollToTopButton scrollRef={scrollRef} />
     </StyledSocialDiv>
   );
 }
