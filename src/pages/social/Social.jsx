e76b693ec71257338a3657a64c4e84f12b2fd473
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-06-16 10:26:13 +0900

    ✨feat: 인스타그램, 네이버 컴포넌트 추가

--- src/pages/social/Social.jsx
+++ src/pages/social/Social.jsx
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
 import Header from "../../components/common/header/Header";
-import GoogleNews from "../../components/common/news/google-news/Google.news";
 import Sidebar from "../../components/common/sidebar/Sidebar";
 import GoogleItem from "./Google.item";
+import InstagramItem from "./Instagram.item";
+import NaverItem from "./NaverItem";
 import { StyledSocialDiv, StyledSocialInfoDiv } from "./Social.style";
 import YoutubeItem from "./Youtube.item";
 
@@ -14,18 +14,9 @@ export default function SocialPage() {
         <Header />
         <GoogleItem />
         <YoutubeItem />
+        <InstagramItem />
+        <NaverItem />
       </StyledSocialInfoDiv>
     </StyledSocialDiv>
   );
-=======
-import YoutubeData from "~/components/common/news/youtube-data/Youtube.data";
-import InstagramData from "../../components/common/news/instagram-data/instagram.data";
-
-export default function SocialPage() {
-  return <div>
-    socialPage
-    <YoutubeData></YoutubeData>
-    <InstagramData></InstagramData>
-    </div>;
->>>>>>> develop
 }
