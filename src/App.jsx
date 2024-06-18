e3d2a50922f568842590de0b9ce9e988f07d4b1e
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-06-18 14:44:36 +0900

    ✨feat: react-query App.js에 적용

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,30 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/landing/Landing";
 import MainPage from "./pages/main/Main";
 import Socialpage from "./pages/social/Social";
-import StockPage from "./pages/stock/detail/maintab/Maintab";
+import Maintab from "./pages/stock/detail/maintab/Maintab";
+import StockPage from "./pages/stock/Stock";
 import KeywordPage from "./pages/keyword/Keyword";
-import StockDetailPage from "./pages/stock/detail/Stock.detail";
-
+import { QueryClient, QueryClientProvider } from "react-query";
 import Pricetab from "./pages/stock/detail/pricetab/Pricetab";
+
 function App() {
+  const queryClient = new QueryClient();
+
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />}></Route>
-          <Route path="/main" element={<MainPage />}></Route>
-          <Route path="/main/social" element={<Socialpage />}></Route>
-          <Route path="/main/stock" element={<StockPage />}></Route>
-          <Route path="/main/stocks/:id" element={<StockDetailPage />}></Route>
-          <Route path="/main/keyword" element={<KeywordPage />}></Route>
-
-          <Route path='/main/stock/dailyprice' element={<Pricetab/>}></Route>
-        </Routes>
-      </Router>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/main" element={<MainPage />} />
+            <Route path="/main/social" element={<Socialpage />} />
+            <Route path="/main/stock" element={<StockPage />} />
+            <Route path="/main/stocks/:id" element={<Maintab />} />
+            <Route path="/main/keyword" element={<KeywordPage />} />
+            <Route path="/main/stocks/:id/dailyprice" element={<Pricetab />} />
+          </Routes>
+        </Router>
+      </QueryClientProvider>
     </div>
   );
 }
