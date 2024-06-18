336b30b651860651503b6784b31438e7e0a32682
박찬란 <164446778+colde99@users.noreply.github.com>
2024-06-18 21:27:26 +0900

    ✨feat: 연관키워드 버블 구현완료

--- src/pages/keyword/Keyword.jsx
+++ src/pages/keyword/Keyword.jsx
@@ -1,3 +1,21 @@
+import React from "react";
+import { useRef } from "react";
+import RelatedKeyword from "../../components/common/bubble-relatedkeyword/RelatedKeyword";
+import Sidebar from "../../components/common/sidebar/Sidebar";
+import Header from "../../components/common/header/Header";
+import { StyledSocialDiv,StyledSocialInfoDiv } from "../social/Social.style";
+
+
 export default function KeywordPage() {
-  return <div>keywordPage</div>;
+  const scrollRef = useRef(null);
+
+  return (  <StyledSocialDiv>
+      <Sidebar />
+      <StyledSocialInfoDiv ref={scrollRef}>
+        <Header />
+      <RelatedKeyword></RelatedKeyword>
+      </StyledSocialInfoDiv>
+      </StyledSocialDiv>
+  )
+  ;
 }
