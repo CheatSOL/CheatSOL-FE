738bf8ef1e066c1bd4def57e5c9d872df1efd5ab
gaeon <rkdjs1104@ajou.ac.kr>
2024-06-19 08:16:16 +0900

    ✨feat: App.jsx-router 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/landing/Landing";
 import MainPage from "./pages/main/Main";
 import Socialpage from "./pages/social/Social";
-import StockPage from "./pages/stock/detail/maintab/Maintab";
+import Maintab from "./pages/stock/detail/maintab/Maintab";
 import KeywordPage from "./pages/keyword/Keyword";
 import StockDetailPage from "./pages/stock/detail/Stock.detail";
-
+import Newstab from "./pages/stock/detail/newstab/Newstab";
 import Pricetab from "./pages/stock/detail/pricetab/Pricetab";
+
 function App() {
   return (
     <div className="App">
@@ -16,11 +17,12 @@ function App() {
           <Route path="/" element={<LandingPage />}></Route>
           <Route path="/main" element={<MainPage />}></Route>
           <Route path="/main/social" element={<Socialpage />}></Route>
-          <Route path="/main/stock" element={<StockPage />}></Route>
+          {/* <Route path="/main/stock/" element={<StockPage />}></Route> */}
           <Route path="/main/stocks/:id" element={<StockDetailPage />}></Route>
           <Route path="/main/keyword" element={<KeywordPage />}></Route>
-
+          <Route path='/main/stock/news' element={<Newstab/>}></Route>
           <Route path='/main/stock/dailyprice' element={<Pricetab/>}></Route>
+          <Route path="/main/stock/total" element={<Maintab/>}></Route>
         </Routes>
       </Router>
     </div>
