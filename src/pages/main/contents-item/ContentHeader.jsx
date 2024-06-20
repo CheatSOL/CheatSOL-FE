a3fc6007bf142a733cc151e3c7c6b8522fb4c32d
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2024-06-20 19:46:59 +0900

    ✨feat: ContentHeader.jsx - 연관 주식 탭으로 가는 네비게이션 추가
    #54

--- src/pages/main/contents-item/ContentHeader.jsx
+++ src/pages/main/contents-item/ContentHeader.jsx
@@ -1,20 +1,53 @@
 import React from "react";
-import { HeaderContentDiv } from "./ContentHeader.style";
+import { HeaderContentDiv, GlowIcon } from "./ContentHeader.style";
+import { HiChevronDoubleRight } from "react-icons/hi";
+import { Link, useLocation } from "react-router-dom";
 
 export default function ContentHeader(props) {
+  const location = useLocation();
   return (
     <HeaderContentDiv>
-      <img style={{ width: "60px", height: "60px" }} src={props.imgUrl}></img>
-      <span style={{ fontSize: "24px", color: "#2E2E30" }}>
-        <strong
-          style={{
-            fontSize: "30px",
-          }}
-        >
-          "{props.keyword}"
-        </strong>
-        {props.description}
-      </span>
+      <div
+        style={{
+          textAlign: "top",
+          alignItems: "center",
+          justifyContent: "center",
+          display: "flex",
+        }}
+      >
+        <img style={{ width: "60px", height: "60px" }} src={props.imgUrl}></img>
+        <span style={{ fontSize: "24px", color: "#2E2E30" }}>
+          <strong
+            style={{
+              fontSize: "30px",
+            }}
+          >
+            "{props.keyword}"
+          </strong>
+          {props.description}
+        </span>
+      </div>
+      <div
+        style={{
+          textAlign: "top",
+          alignItems: "center",
+          justifyContent: "center",
+          display: "flex",
+        }}
+      >
+        <Link to="/main/stock">
+          <GlowIcon>
+            <HiChevronDoubleRight
+              style={{
+                cursor: "pointer",
+                width: "40px",
+                height: "40px",
+                color: "#00537A",
+              }}
+            ></HiChevronDoubleRight>
+          </GlowIcon>
+        </Link>
+      </div>
     </HeaderContentDiv>
   );
 }
