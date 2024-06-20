6148369c18796dd3695d07d76cd654fbdf40a0cd
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-06-20 15:29:59 +0900

    ✨ feat: 연관 주식 페이지 구현 완료

--- src/pages/stock/Stock.style.js
+++ src/pages/stock/Stock.style.js
@@ -9,6 +9,7 @@ export const StyledStockDiv = styled.div`
 export const StyledStockHeaderDiv = styled.div`
   margin-left: 50px;
   font-size: 18px;
+  margin-bottom: 40px;
 
   & strong {
     font-size: 22px;
@@ -26,16 +27,17 @@ export const StyledStockHeaderDiv = styled.div`
 export const StyledStockBodyDiv = styled.div`
   width: 100%;
   display: flex;
-  justify-content: center;
-  align-items: center;
+  justify-content: space-between;
+  align-items: start;
 `;
 
 export const StyledBodyCompanyDiv = styled.div`
-  width: 50%;
+  width: 100%;
   display: flex;
+  justify-content: center;
   flex-wrap: wrap;
   gap: 10px;
-  height: 430px;
+  height: 400px;
   overflow-y: scroll;
 
   &:hover {
@@ -44,10 +46,45 @@ export const StyledBodyCompanyDiv = styled.div`
 `;
 
 export const StyledLoadingDiv = styled.div`
-  width: 650px;
-  height: 450px;
+  width: 800px;
+  height: 400px;
   display: flex;
   justify-content: center;
   align-items: center;
   background-color: white;
+`;
+
+export const StyledContentsDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: ${(props) => props.width || "377px"};
+  height: ${(props) => props.height || "196px"};
+  padding: 30px;
+
+  border: 1px solid #d3d3d3;
+  border-radius: 50px;
+  background-color: white;
+`;
+
+export const StyledHeaderChart = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 50px;
+  & > span {
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    padding: 8px 80px;
+    border-radius: 50px;
+    background-color: white;
+    box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.2);
+  }
+`;
+
+export const StyledSearchSpan = styled.span`
+  color: #ec4b36;
+  font-weight: bold;
+`;
+export const StyledPriceSpan = styled.span`
+  color: #43d2ff;
+  font-weight: bold;
 `;
