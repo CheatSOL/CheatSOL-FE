194b54dadc29b9b2c2af5737d32ffbeaf36b7f1d
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-06-20 15:31:46 +0900

    ♻️ refactor: 컨텐츠 컴포넌트 api 연동 후 구현 완료

--- src/components/common/contents-item/Contents.style.js
+++ src/components/common/contents-item/Contents.style.js
@@ -6,14 +6,14 @@ export const StyledContentsDiv = styled.div`
   width: ${(props) => props.width || "377px"};
   height: ${(props) => props.height || "196px"};
   padding: 30px;
-  
-  border: 1px solid #D3D3D3;
+
+  border: 1px solid #d3d3d3;
   border-radius: 50px;
   background-color: white;
 `;
 
 // TitleGroup
-export const StyledContentsTitleGroup =styled.div`
+export const StyledContentsTitleGroup = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -21,46 +21,44 @@ export const StyledContentsTitleGroup =styled.div`
 
   font-weight: 800;
   font-size: 1.5rem;
-`
+`;
 // Title & SubTitle & MiniTitle
-export const StyledContentsTitle =styled.div`
-`
-export const StyledContentsSubTitle =styled.div`
-`
-export const StyledContentsMiniTitle =styled.div`
+export const StyledContentsTitle = styled.div``;
+export const StyledContentsSubTitle = styled.div``;
+export const StyledContentsMiniTitle = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   gap: 9px;
 
-  & p{
+  & p {
     font-size: 0.9rem !important;
     font-weight: 500;
-    color: orangered;
+    color: ${(props) => (props.isPriceIncrease ? "tomato" : "#4284F3")};
   }
 
-  & img{
+  & img {
     height: 10px;
   }
-`
+`;
 
 // Tag
-export const StyledContentsTag =styled.div`
+export const StyledContentsTag = styled.div`
   display: flex;
   gap: 10px;
   font-size: 0.7rem !important;
   margin-top: 10px;
 
-  & .price-box{
+  & .price-box {
     display: flex;
     gap: 5px;
     padding: 5px;
-    background-color: rgb(211,211,211,0.3);
-    border: 1px solid rgb(211,211,211,0.3);
+    background-color: rgb(211, 211, 211, 0.3);
+    border: 1px solid rgb(211, 211, 211, 0.3);
     border-radius: 8px;
   }
 
-  & p{
+  & p {
     color: orangered;
   }
-`
+`;
