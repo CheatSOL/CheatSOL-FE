127c71d827467e903cc4ac10533dd66d9354dbc7
gaeon <rkdjs1104@ajou.ac.kr>
2024-06-21 23:48:56 +0900

    🎨design: ChartModal - 차트 모달 디자인 추가

--- src/pages/stock/detail/chart/ChartModal.jsx
+++ src/pages/stock/detail/chart/ChartModal.jsx
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal'; // Import Modal from react-bootstrap
 import StockChart from './StockChart';
 
-function ChartModal({ show, onHide }) { // Destructure props correctly
+function ChartModal({ show, onHide, code, name}) { // Destructure props correctly
   return (
     <>
       <Modal
@@ -15,12 +15,17 @@ function ChartModal({ show, onHide }) { // Destructure props correctly
         centered
       >
         <Modal.Header closeButton>
-          <Modal.Title id="example-custom-modal-styling-title">
-            00593 삼성전자
+        
+          <Modal.Title id="example-custom-modal-styling-title">    
+            <span style={{fontSize:"30px"}}>{code}</span>       <span style={{fontSize:"23x", marginLeft:"10px"}}>{name}</span>
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
+        
           <StockChart />
+          
+          <div style={{fontSize:"14px", display:"flex", justifyContent:"end"}}><i class="bi bi-info-circle" style={{marginRight:"5px"}}></i>일별/주별/월별 최근 30개의 정보를 제공합니다.</div>
+          
         </Modal.Body>
       </Modal>
     </>
