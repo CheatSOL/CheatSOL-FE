d128ac7ec18cda10509cf3985f427ed88b05b7cf
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-06-24 09:35:55 +0900

    ✨ feat: redux에 다크 모드 상태값 추가 완료

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import keywordReducer from "./reducers/search";
+import themeReducer from "./reducers/darkmode";
 
 const rootReducer = combineReducers({
   keyword: keywordReducer,
+  theme: themeReducer,
 });
 const store = configureStore({
   reducer: rootReducer,
