0c46d2b0c6668efa70ea9a8f6050435db33f3842
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-06-26 14:49:44 +0900

    ✨ feat: 소셜페이지 인스타그램 컴포넌트 다크모드 적용

--- src/pages/social/Instagram.item.jsx
+++ src/pages/social/Instagram.item.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState, useEffect, useMemo } from "react";
 import InstagramGraph from "../../components/common/keywordGraph/instagramGraph/InstagramGraph";
 import InstagramData from "../../components/common/news/instagram-data/instagram.data";
 import InstagramHotHashTags from "../../components/common/keywordGraph/instagramGraph/InstagramHotHashTags";
@@ -12,8 +12,8 @@ import {
 import { getInstagramSocialTrend } from "../../lib/apis/social";
 
 export default function InstagramItem() {
+  const darkMode = useSelector((state) => state.theme.darkMode);
   const [lineData, setLineData] = useState({});
-  const [options, setOptions] = useState({});
   const [data, setData] = useState([]);
   const [topTags, setTopTags] = useState([]);
   const [tagInfo, setTagInfo] = useState([]);
@@ -36,69 +36,6 @@ export default function InstagramItem() {
           return item.posts - instagramInfo.trendData[index - 1].posts;
         });
 
-        const maxDataValue = Math.max(...changes);
-        const minDataValue = Math.min(...changes);
-        const range = Math.max(Math.abs(maxDataValue), Math.abs(minDataValue));
-        const symRange = range;
-
-        const formatYAxisLabel = (value) => {
-          if (Math.abs(value) >= 1000000) {
-            return (Math.abs(value) / 1000000).toFixed(1) + "M";
-          } else if (Math.abs(value) >= 1000) {
-            return (Math.abs(value) / 1000).toFixed(0) + "K";
-          }
-          return value;
-        };
-
-        const updatedOptions = {
-          scales: {
-            y1: {
-              beginAtZero: true,
-              position: "left",
-              grid: {
-                drawOnChartArea: false,
-              },
-              ticks: {
-                callback: (value) => formatYAxisLabel(value),
-              },
-            },
-            y2: {
-              beginAtZero: true,
-              position: "right",
-              grid: {
-                drawTicks: true,
-                display: true,
-              },
-              ticks: {
-                callback: (value) => {
-                  let formattedValue = formatYAxisLabel(Math.abs(value));
-                  if (value > 0) {
-                    return `${formattedValue}`;
-                  } else if (value < 0) {
-                    return `-${formattedValue}`;
-                  }
-                  return formattedValue;
-                },
-              },
-              min: -symRange,
-              max: symRange,
-            },
-            x: {
-              beginAtZero: true,
-            },
-          },
-          maintainAspectRatio: false,
-          plugins: {
-            legend: {
-              position: "top",
-            },
-            tooltip: {
-              mode: "index",
-              intersect: false,
-            },
-          },
-        };
-
         setLineData({
           labels: labels,
           datasets: [
@@ -112,7 +49,9 @@ export default function InstagramItem() {
             {
               label: "전월 대비 증감",
               data: changes,
-              backgroundColor: "rgba(214, 41, 118, 0.2)",
+              backgroundColor: darkMode
+                ? "rgba(214, 41, 118, 0.7)"
+                : "rgba(214, 41, 118, 0.2)",
               borderRadius: 20,
               barThickness: 20,
               yAxisID: "y2",
@@ -120,14 +59,48 @@ export default function InstagramItem() {
             },
           ],
         });
-
-        setOptions(updatedOptions);
       } catch (error) {
         console.error("Error fetching Instagram data:", error);
       }
     }
     fetchData(keyword);
   }, [keyword]);
+
+  // Dark mode 변경 시 lineData 업데이트
+  useEffect(() => {
+    if (data.length > 0) {
+      const labels = data.map((item) => item.date);
+      const counts = data.map((item) => item.posts);
+      const changes = data.map((item, index) => {
+        if (index === 0) return 0;
+        return item.posts - data[index - 1].posts;
+      });
+
+      setLineData({
+        labels: labels,
+        datasets: [
+          {
+            label: "포스트 수",
+            data: counts,
+            borderColor: "rgba(214, 41, 118, 1)",
+            borderWidth: 3,
+            yAxisID: "y1",
+          },
+          {
+            label: "전월 대비 증감",
+            data: changes,
+            backgroundColor: darkMode
+              ? "rgba(214, 41, 118, 0.7)"
+              : "rgba(214, 41, 118, 0.2)",
+            borderRadius: 20,
+            barThickness: 20,
+            yAxisID: "y2",
+            type: "bar",
+          },
+        ],
+      });
+    }
+  }, [darkMode, data]);
 
   const scrollRef = useRef(null);
   const [isGraphVisible, setIsGraphVisible] = useState(false);
@@ -154,18 +127,93 @@ export default function InstagramItem() {
     };
   }, []);
 
+  const options = useMemo(() => {
+    const maxDataValue = Math.max(...(lineData.datasets?.[1]?.data || []));
+    const minDataValue = Math.min(...(lineData.datasets?.[1]?.data || []));
+    const range = Math.max(Math.abs(maxDataValue), Math.abs(minDataValue));
+    const symRange = range;
+
+    const formatYAxisLabel = (value) => {
+      if (Math.abs(value) >= 1000000) {
+        return (Math.abs(value) / 1000000).toFixed(1) + "M";
+      } else if (Math.abs(value) >= 1000) {
+        return (Math.abs(value) / 1000).toFixed(0) + "K";
+      }
+      return value;
+    };
+
+    return {
+      scales: {
+        y1: {
+          beginAtZero: true,
+          position: "left",
+          grid: {
+            drawOnChartArea: false,
+          },
+          ticks: {
+            color: darkMode ? "white" : "black", // y1축 텍스트 색상 설정
+            callback: (value) => formatYAxisLabel(value),
+          },
+        },
+        y2: {
+          beginAtZero: true,
+          position: "right",
+          grid: {
+            drawTicks: true,
+            display: true,
+          },
+          ticks: {
+            color: darkMode ? "white" : "black", // y2축 텍스트 색상 설정
+            callback: (value) => {
+              let formattedValue = formatYAxisLabel(Math.abs(value));
+              if (value > 0) {
+                return `${formattedValue}`;
+              } else if (value < 0) {
+                return `-${formattedValue}`;
+              }
+              return formattedValue;
+            },
+          },
+          min: -symRange,
+          max: symRange,
+        },
+        x: {
+          beginAtZero: true,
+          ticks: {
+            color: darkMode ? "white" : "black", // x축 텍스트 색상 설정
+          },
+        },
+      },
+      maintainAspectRatio: false,
+      plugins: {
+        legend: {
+          position: "top",
+        },
+        tooltip: {
+          mode: "index",
+          intersect: false,
+        },
+      },
+    };
+  }, [darkMode, lineData]);
+
   return (
-    <StyledSocialInstagramDiv>
+    <StyledSocialInstagramDiv darkMode={darkMode}>
       <StyledInstagramItemDiv>
         <div>
           <img src="/assets/images/instagram-logo.png" alt="Instagram Logo" />
         </div>
         <div></div>
       </StyledInstagramItemDiv>
-      <StyledInstagramChartNewsDiv ref={scrollRef}>
+      <StyledInstagramChartNewsDiv ref={scrollRef} darkMode={darkMode}>
         <div style={{ marginTop: "18px" }}>
           <div>
-            <span style={{ fontSize: "16px" }}>
+            <span
+              style={{
+                fontSize: "16px",
+                color: darkMode ? "white" : "rgba(0, 0, 0, 0.7)",
+              }}
+            >
               <strong>"{keyword}"</strong>이 이만큼 언급됐어요
             </span>
             {isGraphVisible && data ? (
@@ -179,7 +227,12 @@ export default function InstagramItem() {
             )}
           </div>
           <div>
-            <span style={{ fontSize: "18px" }}>
+            <span
+              style={{
+                fontSize: "18px",
+                color: darkMode ? "white" : "rgba(0, 0, 0, 0.7)",
+              }}
+            >
               <strong>"{keyword}"</strong>과 함께 반응이 좋은 해시태그
             </span>
             <InstagramHotHashTags topTags={topTags} />
