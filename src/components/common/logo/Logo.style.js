724f1cf6ad01113b811636f7da57d8f52c1dde35
gaeon <rkdjs1104@ajou.ac.kr>
2024-06-27 17:17:35 +0900

    🎨design: 로고 이미지 수정 및 다크 모드 적용

--- src/components/common/logo/Logo.style.js
+++ src/components/common/logo/Logo.style.js
@@ -4,4 +4,8 @@ export const StyledLogoImg = styled.img`
   width: ${({ size }) =>
     size === "s" ? "50px" : size === "m" ? "150px" : "150px"};
   aspect-ratio: "4/2";
+  margin-Top:20px;
+  margin-Bottom:50px;
+  width:170px;
+  
 `;
